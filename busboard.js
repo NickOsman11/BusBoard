bc94144eb9715a15ae1c3d02a3fd74d8e87d985f
Nick Osman <Nick.Osman@softwire.com>
2022-08-11 16:11:02 +0100

    first five busses

--- busboard.js
+++ busboard.js
@@ -1,23 +1,36 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const readline = require('readline-sync')
-const response = await fetch(url)
-const busData = await response.json
 
-function getBusTimes(){
 
-    console.log("enter a stop code")
-    stopCode = readline.prompt()
+async function getBusTimes(){
+
+    // console.log("enter a stop code")
+    // stopCode = readline.prompt()
+    let stopCode = "490015367S";
 
     var url = "https://api.tfl.gov.uk/StopPoint/" + stopCode + "/Arrivals"
 
+    var busData = await fetch(url).then(response => response.json())
+    // console.log(busData)
+
+    sortedBusData = busData.sort(function(first, second) {
+        return first.timeToStation - second.timeToStation;
+    });
+        
+    for (let i = 0; i<5; i++){
+        console.log(sortedBusData[i].lineName)
+
+    }
 
 
-    console.log(busData)
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(body => (console.log(body));
 
-    // for (let i = 0; i<5; i++){
-    //     console.log(busData[i].timeToStation)
-    // }
+
+
 }
+
 
 getBusTimes()
 
