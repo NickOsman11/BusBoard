feff51f94bb321947b74f27b3d4be9d68a7e8d61
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-12 10:53:55 +0100

    revert to working function

--- busboard.js
+++ busboard.js
@@ -1,11 +1,6 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const readline = require('readline-sync')
 
-async function getUrlData(url){
-    let busData = await fetch(url).then(response => response.json())
-    return busData
-}
-
 async function getBusTimes(){
 
     // console.log("enter a stop code")
@@ -14,25 +9,19 @@ async function getBusTimes(){
 
     var url = "https://api.tfl.gov.uk/StopPoint/" + stopCode + "/Arrivals"
 
-    var busData = getUrlData(url)
+    let busData = await fetch(url).then(response => response.json())
     // console.log(busData)
 
     console.log(busData)
 
-    // sortedBusData = busData.sort(function(first, second) {
-    //     return first.timeToStation - second.timeToStation;
-    // });
+    sortedBusData = busData.sort(function(first, second) {
+        return first.timeToStation - second.timeToStation;
+    });
         
-    // for (let i = 0; i<5; i++){
-    //     console.log(sortedBusData[i].lineName)
+    for (let i = 0; i<5; i++){
+        console.log(sortedBusData[i].lineName)
 
-    // }
-
-
-    // fetch(url)
-    //     .then(response => response.json())
-    //     .then(body => (console.log(body));
-
+    }
 
 
 }
